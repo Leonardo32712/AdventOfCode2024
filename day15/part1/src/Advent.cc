7ac149d9d47d5ce4a5c94a7a709e0228c09ade3c
Leonardo Cruz Rodríguez <Leo1202327@gmail.com>
2024-12-15 04:42:25 +0000

    day 5

--- day15/part1/src/Advent.cc
+++ day15/part1/src/Advent.cc
@@ -5,7 +5,6 @@ int main(int argc, char const *argv[]) {
     fileReader fr(argv[1]);
     RobotSimulator Robots (fr.readRobots());
 
-    //Robots.printRobots();
     std::cout << Robots.getSafetyFactor(100) << std::endl;
 
     return 0;
